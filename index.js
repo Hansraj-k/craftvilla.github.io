7ace1f342036b8f0da2c217fa2e9f41993b5e1c7
Hansraj <82136657+Hansraj-k@users.noreply.github.com>
2021-06-17 19:43:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-	  // preloader
-      window.addEventListener("load", () => {
-        const loader = document.getElementById("pre-loader");
-        setTimeout(() => {
-          loader.classList.add("hide");
-        }, 2000);
-      });
-
-
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("sw.js").then(registration => {
     console.log("SW Registered!");
