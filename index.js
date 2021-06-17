b171839c6d733d79268712946609609d7e1624d6
Hansraj <82136657+Hansraj-k@users.noreply.github.com>
2021-06-17 19:33:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,15 @@
           loader.classList.add("hide");
         }, 2000);
       });
+
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js").then(registration => {
+    console.log("SW Registered!");
+    console.log(registration);
+  }).catch(error => {
+    console.log("SW Registration Failed");
+    console.log(error);
+  })
+}
       
